9c21fbedc6fd9927f46e747d6d78427ad2769d23
matiasmenesess <155995043+matiasmenesess@users.noreply.github.com>
2024-11-13 12:39:51 -0500

    Update problema4.cpp

--- problema4.cpp
+++ problema4.cpp
@@ -2,13 +2,3 @@
 //1. Olivares Sarmiento, Javier Ignacio Leon
 //2. Silva Reyes, Santiago Miguel
 //3. Meneses Roncal, Matias Alonso
-class Solution {
-public:
-
-    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-       for(auto e: edges){ 
-
-       } 
-       return edges[3];
-    }
-};
